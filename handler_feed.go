ba2421310358e3d502b6ff35579c2eb75807a108
Doug Fletcher <dbfletcher@gmail.com>
2025-08-29 10:50:45 -0400

    CH3-L3 final

--- handler_feed.go
+++ handler_feed.go
@@ -38,9 +38,25 @@ func handlerAddFeed(s *state, cmd command) error {
 	return nil
 }
 
+func handlerListFeeds(s *state, cmd command) error {
+	feeds, err := s.db.GetFeeds(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't get feeds: %w", err)
+	}
+
+	for _, feed := range feeds {
+		fmt.Printf("Feed: %s\n", feed.Name)
+		fmt.Printf("URL: %s\n", feed.Url)
+		fmt.Printf("Owner: %s\n", feed.UserName)
+		fmt.Println("---")
+	}
+	return nil
+}
+
 func printFeed(feed database.Feed) {
 	fmt.Printf(" * ID:      %v\n", feed.ID)
 	fmt.Printf(" * Name:    %v\n", feed.Name)
 	fmt.Printf(" * URL:     %v\n", feed.Url)
 	fmt.Printf(" * UserID:  %v\n", feed.UserID)
 }
+
