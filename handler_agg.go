21e5312336eb44a2d1499b68385dbfaf6bab2e4a
Doug Fletcher <dbfletcher@gmail.com>
2025-08-30 10:04:53 -0400

    CH5-L2 Final

--- handler_agg.go
+++ handler_agg.go
@@ -1,16 +1,55 @@
-// handler_agg.go
 package main
 
 import (
 	"context"
 	"fmt"
+	"log"
+	"time"
 )
 
-func handlerAgg(s *state, cmd command) error {
-	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+func scrapeFeeds(s *state) {
+	feed, err := s.db.GetNextFeedToFetch(context.Background())
 	if err != nil {
-		return fmt.Errorf("couldn't fetch feed: %w", err)
+		log.Printf("couldn't get next feed to fetch: %v", err)
+		return
 	}
-	fmt.Printf("%+v\n", feed)
-	return nil
+
+	log.Printf("Fetching feed '%s'...", feed.Name)
+	err = s.db.MarkFeedFetched(context.Background(), feed.ID)
+	if err != nil {
+		log.Printf("couldn't mark feed as fetched: %v", err)
+		return
+	}
+
+	rssFeed, err := fetchFeed(context.Background(), feed.Url)
+	if err != nil {
+		log.Printf("couldn't fetch feed: %v", err)
+		return
+	}
+
+	for _, item := range rssFeed.Channel.Item {
+		log.Printf("Found post: '%s'", item.Title)
+	}
+	log.Printf("Feed '%s' collected, %v posts found", feed.Name, len(rssFeed.Channel.Item))
+}
+
+func handlerAgg(s *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <time_between_reqs>", cmd.Name)
+	}
+
+	durationStr := cmd.Args[0]
+	timeBetweenRequests, err := time.ParseDuration(durationStr)
+	if err != nil {
+		return fmt.Errorf("invalid duration: %w", err)
+	}
+
+	fmt.Printf("Collecting feeds every %s...\n", timeBetweenRequests)
+
+	ticker := time.NewTicker(timeBetweenRequests)
+	// Run immediately, then on each tick
+	scrapeFeeds(s)
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
 }
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -43,3 +43,13 @@ WHERE feed_follows.user_id = $1;
 
 -- name: DeleteFeedFollow :exec
 DELETE FROM feed_follows WHERE user_id = $1 AND feed_id = $2;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at NULLS FIRST
+LIMIT 1;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET last_fetched_at = NOW(), updated_at = NOW()
+WHERE id = $1;
